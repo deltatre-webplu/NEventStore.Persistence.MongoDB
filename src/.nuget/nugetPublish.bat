e044d4a59eecdcda091693da1e686d9f004b982f
Davide Icardi <davide.icardi@gmail.com>
2015-10-03 00:25:05 +0200

    Fix publish nuget script

--- src/.nuget/nugetPublish.bat
+++ src/.nuget/nugetPublish.bat
@@ -2,5 +2,5 @@
 
 nuget pack Webplu-NEventStore.Persistence.MongoDB.5.2.0.nupkg.nuspec
 
-nuget push Webplu-NEventStore.Persistence.MongoDB.5.2.0.nupkg.5.2.0.nupkg -source https://www.myget.org/F/webplu/api/v2
+nuget push Webplu-NEventStore.Persistence.MongoDB.5.2.0.nupkg -source https://www.myget.org/F/webplu/api/v2
 
