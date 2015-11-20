15ac39b122fa59376f28e6a83de5f27fbfe4d5b2
Davide Icardi <davide.icardi@gmail.com>
2015-11-20 19:23:08 +0100

    Updated mongocsharpdriver to 2.1.1

--- src/.nuget/nugetPublish.bat
+++ src/.nuget/nugetPublish.bat
@@ -1,6 +1,6 @@
-﻿nuget setApiKey YOUR-KEY -Source https://www.myget.org/F/webplu/api/v2
+﻿rem nuget setApiKey YOUR-KEY -Source https://www.myget.org/F/webplu/api/v2
 
 nuget pack Webplu-NEventStore.Persistence.MongoDB.nuspec
 
-nuget push Webplu-NEventStore.Persistence.MongoDB.5.2.2.nupkg -source https://www.myget.org/F/webplu/api/v2
+nuget push Webplu-NEventStore.Persistence.MongoDB.5.2.3.nupkg -source https://www.myget.org/F/webplu/api/v2
 
